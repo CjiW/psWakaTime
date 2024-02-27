618fdcb427c6547f2a2c5ae009df7cead036ccf0
CjiW <64598696+CjiW@users.noreply.github.com>
2024-02-27 12:45:20 +0800

    Update home.ts

--- src/pages/api/home.ts
+++ src/pages/api/home.ts
@@ -3,9 +3,9 @@ import { BasicInstance } from ".";
 export async function getList(language?: string) {
   if (language) {
     return await BasicInstance.get(
-      `leaders?country_code=SG&language=${language}`
+      `leaders?lang=${language}`
     );
   } else {
-    return await BasicInstance.get("leaders?country_code=SG");
+    return await BasicInstance.get("leaders");
   }
 }
